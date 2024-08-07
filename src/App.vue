ed221789df2e1e8f53c98e8db6d95a211258867d
Los Desktop <losmosga@foxmail.com>
2024-08-07 14:55:27 +0800

    update the servers

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
+    <BaseHeader/>
     <div class="flex main-container">
-<!--      <BaseSide />-->
+      <!--      <BaseSide />-->
       <div w="full" py="4">
-        <Logos my="4" />
-        <SingleSiteServer></SingleSiteServer>
-        <HelloWorld msg="CYMC 服务器中心(更新中，腐竹很懒ovo)" />
+        <Logos my="4"/>
+        <ServerStatistic></ServerStatistic>
 
+                <ServerStatus></ServerStatus>
+        <el-divider>
+          <el-icon>
+            <star-filled/>
+          </el-icon>
+        </el-divider>
+        <MUAServers></MUAServers>
+        <SingleSiteServer></SingleSiteServer>
+        <HelloWorld msg="CYMC 服务器中心(更新中，腐竹很懒ovo)"/>
+        <el-backtop :bottom="100" :visibility-height="20">
+          <div
+              style="
+        height: 100%;
+        width: 100%;
+        background-color: var(--el-bg-color-overlay);
+        box-shadow: var(--el-box-shadow-lighter);
+        text-align: center;
+        line-height: 40px;
+        color: #1989fa;
+
+      "
+          >
+            <el-icon size="24"><CaretTop /></el-icon>
+          </div>
+        </el-backtop>
       </div>
 
     </div>
 
+
   </el-config-provider>
+
 
 </template>
 
@@ -26,4 +52,6 @@
 }
 </style>
 <script setup lang="ts">
+import {CaretTop, StarFilled} from '@element-plus/icons-vue'
+import TopNavButton from "~/components/layouts/TopNavButton.vue";
 </script>
--- src/components/ServerStatus.vue
+++ src/components/ServerStatus.vue
@@ -1,35 +1,62 @@
 <template>
-  <div>
-    <el-button @click="show = !show">Click Me</el-button>
-
-    <div style="margin-top: 20px">
-      <el-collapse-transition>
-        <div v-show="show" style="height: 400px">
-          <div class="transition-box">el-collapse-transition</div>
-          <div class="transition-box">el-collapse-transition</div>
-        </div>
-      </el-collapse-transition>
-    </div>
-  </div>
+  <p class="text-center demonstration">Card Vertical Layout</p>
+  <el-carousel
+      height="400px"
+      direction="vertical"
+      type="card"
+      :autoplay="true"
+  >
+    <el-carousel-item v-for="server in servers" :key="server.id">
+      <a :href="server.url" :title="server.title">
+        <img :src="server.banner" :alt="server.title" width="60%" height="auto"/>
+      </a>
+    </el-carousel-item>
+  </el-carousel>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const show = ref(true)
-</script>
-
-<style>
-.transition-box {
-  margin-bottom: 10px;
-  width: 200px;
-  height: 100px;
-  border-radius: 4px;
-  background-color: #409eff;
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
   text-align: center;
-  color: #fff;
-  padding: 40px 20px;
-  box-sizing: border-box;
-  margin-right: 20px;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
+
+<script setup lang="ts">
+const servers = [
+  {
+    id: 1,
+    title: 'Hypixel',
+    url: 'https://minecraftlist.com/servers/hypixel.net',
+    banner: 'https://minecraftlist.com/servers/hypixel.net/banner@2x.jpg'
+  },
+  {
+    id: 2,
+    title: 'CYMC',
+    url: 'https://minecraftlist.com/servers/mc.cqmu.online',
+    banner: 'https://minecraftlist.com/servers/mc.cqmu.online/banner@2x.jpg'
+  },
+  {
+    id: 3,
+    title: 'Cubecraft',
+    url: 'https://minecraftlist.com/servers/cubecraft.net',
+    banner: 'https://minecraftlist.com/servers/cubecraft.net/banner@2x.jpg'
+  },
+  {
+    id: 4,
+    title: 'complex',
+    url: 'https://minecraftlist.com/servers/org.mc-complex.com',
+    banner: 'https://minecraftlist.com/servers/org.mc-complex.com/banner@2x.jpg'
+  }
+];
+</script>
